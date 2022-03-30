820a39bb2d47f97a2f7b5bcbd272e8331fe2dddc
Quách Trần Thanh Tuyền <tuyen.qtt.1909@gmail.com>
2022-03-30 09:56:50 +0700

    Initial Commit

---
+++ DemoCoreLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoCoreLocation
+//
+//  Created by tuyen.quach on 30/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
