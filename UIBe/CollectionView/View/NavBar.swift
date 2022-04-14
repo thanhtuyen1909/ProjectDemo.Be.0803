d332c1e0a4d23908f8ca0edf757b9288aa0ef659
thanhtuyen1909 <tuyen.qtt.1909@gmail.com>
2022-04-14 13:34:34 +0700

    Done refactor auto layout to FrameLayoutKit for Navbar

--- UIBe/CollectionView/View/NavBar.swift
+++ UIBe/CollectionView/View/NavBar.swift
@@ -14,24 +14,18 @@ enum NavBarMode {
 }
 
 class NavBar: UIView {
-//    @IBOutlet var contentView: UIView!
-//    @IBOutlet weak var userNameLabel: UILabel!
-//    @IBOutlet weak var userScoreView: UIView!
-//    @IBOutlet weak var userScoreLabel: UILabel!
-//    @IBOutlet weak var lineView: UIView!
-//    
     private lazy var user = User()
+    private let userNameLabel = UILabel()
+    private let userScoreLabel = UILabel()
+    private let scoreImage = UIImageView(image: UIImage(named: "imgScore"))
+    private let lineView = UIView()
     
-    private let contentView1 = UIView()
-    private let userNameLabel1 = UILabel()
-    private let userScoreView1 = UIView()
-    private let userScoreLabel1 = UILabel()
-    private let lineView1 = UIView()
+    private let frameLayout = StackFrameLayout(axis: .vertical)
     
     //MARK: Set color to userNameLabel và userScoreLabel
     private func setColorLabel(color: String) {
-        userNameLabel1.textColor = UIColor(hex: color)
-        userScoreLabel1.textColor = UIColor(hex: color)
+        userNameLabel.textColor = UIColor(hex: color)
+        userScoreLabel.textColor = UIColor(hex: color)
     }
     
     override class func awakeFromNib() {
@@ -49,40 +43,65 @@ class NavBar: UIView {
         commonInit()
     }
     
-    private func commonInit() {
-        Bundle.main.loadNibNamed("NavBar", owner: self, options: nil)
-        addSubview(contentView)
-        contentView1.frame = self.bounds
-        contentView1.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        setupTopView()
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        return frameLayout.sizeThatFits(size)
     }
     
-    //MARK: Set up topView
-    func setupTopView() {
-        lineView1.isHidden = true
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        frameLayout.frame = bounds
+    }
+    
+    private func commonInit() {
+        setupComponents()
         
-        userScoreView1.clipsToBounds = true
-        userScoreView1.layer.cornerRadius = userScoreView.frame.height / 2
+        frameLayout + HStackLayout {
+            $0 + userNameLabel
+            $0.addSpace().flexible()
+            $0 + VStackLayout {
+                $0 + HStackLayout {
+                    ($0 + userScoreLabel)
+                    ($0 + scoreImage).fixedSize = CGSize(width: 25, height: 25)
+                    //$0
+                    $0.padding(top: 5.0, left: 8.0, bottom: 5.0, right: 8.0)
+                    $0.backgroundColor = UIColor(hex: "#04050D", alpha: 0.2)
+                }
+                $0.layer.masksToBounds = true
+                $0.distribution = .center
+                $0.didLayoutSubviews {
+                    $0.layer.cornerRadius = $0.bounds.height / 2.0
+                }
+            }
+            $0.padding(top: 12.0, left: 16.0, bottom: 12.0, right: 16.0)
+        }
+        frameLayout.distribution = .bottom
+        frameLayout + lineView
+        addSubview(frameLayout)
+    }
+    
+    private func setupComponents() {
+        userNameLabel.font = .systemFont(ofSize: 20, weight: .bold)
+        userNameLabel.text = "Chào \(user.name)!"
         
-        userScoreLabel1.text = String(user.score)
-        userNameLabel1.text = "Chào \(user.name)!"
+        lineView.isHidden = true
+        lineView.backgroundColor = .tertiarySystemGroupedBackground
+        lineView.frame.size = CGSize(width: frameLayout.frame.width, height: 1.0)
+        
+        userScoreLabel.text = String(user.score)
+        
         setColorLabel(color: DataViewController().primary_text_color_light)
-        
-        contentView1.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-        contentView1.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
-        
     }
     
     //MARK: Set mode
     func setupNavBarMode(style: NavBarMode) {
         switch style {
         case .dark:
-            contentView1.backgroundColor = .white
-            lineView1.isHidden = false
+            frameLayout.backgroundColor = .white
+            lineView.isHidden = false
             setColorLabel(color: DataViewController().primary_text_color_dark)
         case .light:
-            contentView1.backgroundColor = .clear
-            lineView1.isHidden = true
+            frameLayout.backgroundColor = .clear
+            lineView.isHidden = true
             setColorLabel(color: DataViewController().primary_text_color_light)
         }
     }
